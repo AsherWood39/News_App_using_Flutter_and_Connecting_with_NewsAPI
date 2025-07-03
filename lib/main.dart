c78726fc2acf0f23141b21f517e9f346a57403ab
AsherWood39 <athishtapa05@gmail.com>
2025-07-03 20:37:35 +0530

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_using_newsapi_key/Presentation/home_screen.dart';
 import 'package:news_app_using_newsapi_key/Presentation/splash_screen.dart';
 import 'package:news_app_using_newsapi_key/firebase_options.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CarouselIndexProvider(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
